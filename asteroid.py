a902a32ecccb1fb3b2a6fea53e790d25fe795930
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-08 14:31:24 -0300

    fea asteroids splitting on shot

--- asteroid.py
+++ asteroid.py
@@ -12,3 +12,20 @@ class Asteroid(CircleShape):
 
   def update(self, dt):
       self.position += self.velocity * dt
+
+  def split(self):
+    self.kill()
+
+    if self.radius <= ASTEROID_MIN_RADIUS:
+      return
+
+    new_angle = random.uniform(20, 50)     
+    
+    vector_1 = self.velocity.rotate(new_angle)
+    vector_2 = self.velocity.rotate(-new_angle)
+    
+    new_radius = self.radius - ASTEROID_MIN_RADIUS
+    asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    asteroid.velocity = vector_1 * 1.2
+    asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+    asteroid.velocity = vector_2 * 1.2
