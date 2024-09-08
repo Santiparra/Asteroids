6764496381f6ae5e7f3fffb644074f91553e6f5a
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-08 12:18:42 -0300

    player object with visual representation

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
   pygame.init()
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
   p_clock = pygame.time.Clock()
   dt = 0
+  player = Player(
+    (SCREEN_WIDTH / 2), 
+    (SCREEN_HEIGHT / 2)
+  )
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
+
     screen.fill("black")
+    player.draw(screen)
     pygame.display.flip()
-    dt = p_clock.tick(60) / 1000
+
+    #FPS limiter
+    dt = p_clock.tick(60) / 1000    
 
   print("Starting asteroids!")  
   print(f"Screen width: {SCREEN_WIDTH}")
